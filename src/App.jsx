b99e5395f0e86f695cd2d6eb859eb44687e002d3
Kaya Peng <kpeng09@Kayas-MacBook-Pro.local>
2025-01-23 15:59:46 -0800

    polished part1a exercise

--- src/App.jsx
+++ src/App.jsx
@@ -48,22 +48,6 @@ const App = () => {
       />
     </div>
   )
-
-  // return (
-  //   <div>
-  //     <h1>{course}</h1>
-  //     <p>
-  //       {part1} {exercises1}
-  //     </p>
-  //     <p>
-  //       {part2} {exercises2}
-  //     </p>
-  //     <p>
-  //       {part3} {exercises3}
-  //     </p>
-  //     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-  //   </div>
-  // )
 }
 
 export default App
