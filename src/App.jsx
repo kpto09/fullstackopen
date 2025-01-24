ddc1ae93a1eee62b9b9b52f9b801d06bc9a73e3e
Kaya Peng <kpeng09@Kayas-MacBook-Pro.local>
2025-01-23 17:00:09 -0800

    completed part1.3

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Parts content={props.content1} exercises={props.exercises1} />
-      <Parts content={props.content2} exercises={props.exercises2} />
-      <Parts content={props.content3} exercises={props.exercises3} />
+      <Parts content={props.part1.name} exercises={props.part1.exercises} />
+      <Parts content={props.part2.name} exercises={props.part2.exercises} />
+      <Parts content={props.part3.name} exercises={props.part3.exercises} />
     </div>
   )
 }
@@ -33,24 +33,32 @@ const Total = (props) => {
 const App = () => {
   const course = 'Half Stack application development'
 
-  const content = [
-    { part: 'Fundamentals of React', exercises: 10 },
-    { part: 'Using props to pass data', exercises: 7 },
-    { part: 'State of a component', exercises: 14}
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content 
-        content1={content[0].part} exercises1={content[0].exercises}
-        content2={content[1].part} exercises2={content[1].exercises}
-        content3={content[2].part} exercises3={content[2].exercises}
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
       />
       <Total 
-        exercises1={content[0].exercises}
-        exercises2={content[1].exercises}
-        exercises3={content[2].exercises}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
       />
     </div>
   )
