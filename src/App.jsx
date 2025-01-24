da5765de0bbfcdc22ea4c6518721b1932f676d8d
Kaya Peng <kpeng09@Kayas-MacBook-Pro.local>
2025-01-23 22:21:18 -0800

    part1.c destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,27 @@
-const Header = (props) => {
+const Hello = ({ name, age }) => {
+
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <h2>{props.course}</h2>
-    </div>
-  )
-}
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
 
-const Content = (props) => {
-  return (
-    <div>
-      <Parts content={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Parts content={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Parts content={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Parts = (props) => {
-  return (
-    <div>{props.content} {props.exercises}</div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Total Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content
-        parts={course.parts}
-      />
-      <Total 
-        parts={course.parts}
-      />
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
