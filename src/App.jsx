ed65423e5c5be42aa5606d372f496b33dc652ade
Kaya Peng <kpeng09@Kayas-MacBook-Pro.local>
2025-01-24 19:00:51 -0800

    completed part1.7 and 1.8

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,110 @@ import { useState } from 'react'
 
 const Display = ({title}) => <div> <h2>{title}</h2></div>
 
-const Statistic = ({value, text}) => <div>{text} {value}</div>
-
+const Statistic = ({value, text}) =>  { 
+  if (text === 'positive') {
+    return (
+      <div> {text} {value} %</div>
+    )
+  }
+  return (
+    <div>{text} {value}</div>
+  )
+}
 const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
 
-  const goodCounter = () => setGood(good + 1);
-  const neutralCounter = () => setNeutral(neutral + 1);
-  const badCounter = () => setBad(bad + 1);
+  const [stats, setStats] = useState({
+    all: 0,
+    average: 0,
+    positive: 0
+  })
+
+  const handleGoodClick = () => {
+    const updatedGood = clicks.good + 1
+    const newClick = {
+      ...clicks,
+      good: updatedGood
+    }
+
+    setClicks(newClick)
+    
+    const updatedAll = updatedGood + clicks.neutral + clicks.bad
+    const updatedAverage = (updatedGood - clicks.bad) / updatedAll
+    const updatedPositive = (updatedGood / updatedAll) * 100
+
+    const newStats = {
+      all: updatedAll,
+      average: updatedAverage,
+      positive: updatedPositive
+    }
+
+    setStats(newStats)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = clicks.neutral + 1
+    const newClick = {
+      ...clicks,
+      neutral: updatedNeutral
+    }
+    setClicks(newClick);
+
+    const updatedAll = clicks.good + updatedNeutral + clicks.bad
+    const updatedPositive = (clicks.good / updatedAll) * 100
+
+    const newStats = {
+      ...stats,
+      all: updatedAll,
+      positive: updatedPositive
+    }
+
+    setStats(newStats)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = clicks.bad + 1
+  
+    const newClick = {
+      ...clicks,
+      bad: updatedBad
+    }
+    setClicks(newClick);
+
+    const updatedAll = clicks.good + clicks.neutral + updatedBad
+    const updatedAverage = (clicks.good - updatedBad) / updatedAll
+    const updatedPositive = (clicks.good / updatedAll) * 100
+
+    const newStats = {
+      all: updatedAll,
+      average: updatedAverage,
+      positive: updatedPositive
+    }
+  
+    setStats(newStats)
+  }
+
 
   return (
     <div>
       <Display title='give feedback' />
-      <Button onClick={goodCounter} text='good'/>
-      <Button onClick={neutralCounter} text='neutral'/>
-      <Button onClick={badCounter} text='bad'/>
+      <Button onClick={handleGoodClick} text='good'/>
+      <Button onClick={handleNeutralClick} text='neutral'/>
+      <Button onClick={handleBadClick} text='bad'/>
+
       <Display title='statistics' />
-      <Statistic value={good} text='good'/>
-      <Statistic value={neutral} text='neutral'/>
-      <Statistic value={bad} text='bad'/>
+      <Statistic value={clicks.good} text='good'/>
+      <Statistic value={clicks.neutral} text='neutral'/>
+      <Statistic value={clicks.bad} text='bad'/>
+      <Statistic value={stats.all} text='all'/>
+      <Statistic value={stats.average} text='average'/>
+      <Statistic value={stats.positive} text='positive'/>
     </div>
   )
 }
