bb30586d115b16cae153bade92f5341a13fe5964
Kaya Peng <kpeng09@Kayas-MacBook-Pro.local>
2025-01-25 18:10:26 -0800

    completed part1.9-1.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
 import { useState } from 'react'
 
-const Display = ({title}) => <div> <h2>{title}</h2></div>
+// todo: fix statistics and button logic
 
-const Statistic = ({value, text}) =>  { 
+const Display = ({title}) => <div><h2>{title}</h2></div>
+
+const StatisticLine = ({text, value}) => {
   if (text === 'positive') {
     return (
-      <div> {text} {value} %</div>
+      <tr>
+      <td>{text}</td> 
+      <td>{value} %</td>
+    </tr>
     )
   }
   return (
-    <div>{text} {value}</div>
+    <tr>
+      <td>{text}</td> 
+      <td>{value}</td>
+    </tr>
   )
+}
+
+const Statistic = ({clicks, text}) =>  {
+  const total = clicks.good + clicks.neutral + clicks.bad
+  const average = (clicks.good * 1 + clicks.bad * -1) / total
+  const positive = clicks.good * (100/total)
+
+  if (total === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text={text.good} value={clicks.good}/>
+        <StatisticLine text={text.neutral} value={clicks.neutral}/>
+        <StatisticLine text={text.bad} value={clicks.bad}/>
+        <StatisticLine text='all' value={total}/>
+        <StatisticLine text='average' value={average}/>
+        <StatisticLine text='positive' value={positive}/>
+      </tbody>     
+    </table>
+  )
+  
 }
 const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>
 
@@ -18,77 +52,33 @@ const App = () => {
   const [clicks, setClicks] = useState({
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
   })
 
-  const [stats, setStats] = useState({
-    all: 0,
-    average: 0,
-    positive: 0
-  })
-
-  const handleGoodClick = () => {
-    const updatedGood = clicks.good + 1
-    const newClick = {
-      ...clicks,
-      good: updatedGood
-    }
-
-    setClicks(newClick)
-    
-    const updatedAll = updatedGood + clicks.neutral + clicks.bad
-    const updatedAverage = (updatedGood - clicks.bad) / updatedAll
-    const updatedPositive = (updatedGood / updatedAll) * 100
-
-    const newStats = {
-      all: updatedAll,
-      average: updatedAverage,
-      positive: updatedPositive
-    }
-
-    setStats(newStats)
+  const text = {
+    good: 'good',
+    neutral: 'neutral',
+    bad: 'bad',
   }
 
-  const handleNeutralClick = () => {
-    const updatedNeutral = clicks.neutral + 1
-    const newClick = {
+  const handleGoodClick = () => {
+    setClicks({
       ...clicks,
-      neutral: updatedNeutral
-    }
-    setClicks(newClick);
-
-    const updatedAll = clicks.good + updatedNeutral + clicks.bad
-    const updatedPositive = (clicks.good / updatedAll) * 100
-
-    const newStats = {
-      ...stats,
-      all: updatedAll,
-      positive: updatedPositive
-    }
-
-    setStats(newStats)
+      good: clicks.good +1
+    })
+  }
+  const handleNeutralClick = () => {
+    setClicks({
+      ...clicks,
+      neutral: clicks.neutral +1
+    })
   }
 
   const handleBadClick = () => {
-    const updatedBad = clicks.bad + 1
-  
-    const newClick = {
+    setClicks({
       ...clicks,
-      bad: updatedBad
-    }
-    setClicks(newClick);
-
-    const updatedAll = clicks.good + clicks.neutral + updatedBad
-    const updatedAverage = (clicks.good - updatedBad) / updatedAll
-    const updatedPositive = (clicks.good / updatedAll) * 100
-
-    const newStats = {
-      all: updatedAll,
-      average: updatedAverage,
-      positive: updatedPositive
-    }
-  
-    setStats(newStats)
+      bad: clicks.bad +1
+    })
   }
 
 
@@ -100,12 +90,7 @@ const App = () => {
       <Button onClick={handleBadClick} text='bad'/>
 
       <Display title='statistics' />
-      <Statistic value={clicks.good} text='good'/>
-      <Statistic value={clicks.neutral} text='neutral'/>
-      <Statistic value={clicks.bad} text='bad'/>
-      <Statistic value={stats.all} text='all'/>
-      <Statistic value={stats.average} text='average'/>
-      <Statistic value={stats.positive} text='positive'/>
+      <Statistic clicks={clicks} text={text}/>
     </div>
   )
 }
